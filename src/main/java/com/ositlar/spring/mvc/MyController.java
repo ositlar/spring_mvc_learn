9ec5839cc1587d135cdc1c8f9dd3677f7e6f6bea
ositlar <mprom13@icloud.com>
2023-12-01 18:18:53 +0600

    fourth lesson

--- src/main/java/com/ositlar/spring/mvc/MyController.java
+++ src/main/java/com/ositlar/spring/mvc/MyController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
@@ -19,9 +21,17 @@ public class MyController {
         return "askEmpDetailsView";
     }
 
-    @RequestMapping("/showDetails")
+    /*@RequestMapping("/showDetails")
     public String showEmployeeDetails(HttpServletRequest request, Model model) {
         String empName= request.getParameter("employeeName");
+        empName = "Mr. " + empName;
+        model.addAttribute("nameAttribute", empName);
+        model.addAttribute("description", " - udemy student");
+        return "showEmpDetails";
+    }*/
+
+    @RequestMapping("/showDetails")
+    public String showEmployeeDetails(@RequestParam("employeeName") String empName, Model model) {
         empName = "Mr. " + empName;
         model.addAttribute("nameAttribute", empName);
         model.addAttribute("description", " - udemy student");
